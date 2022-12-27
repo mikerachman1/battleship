b4912af5a434b6508c4d4926b1101ab7e75e0f8c
Mike Rachman <mikerachman1@gmail.com>
2022-12-27 08:59:00 -0500

    wrote tests for isSunk function

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,9 +1,21 @@
 /* eslint-disable */
-import { shipFactory, hit } from "./ship";
+import { shipFactory, hit, isSunk } from "./ship";
 
 test('calling hit with ship returns new ship hit count', () => {
   let carrier = shipFactory(5);
   expect(carrier.hits).toBe(0);
   carrier.hits = hit(carrier);
   expect(carrier.hits).toBe(1);
+});
+
+test('calling isSunk returns true for hits === length', () => {
+  let submarine = shipFactory(3, 3);
+  submarine.isSunk = isSunk(submarine);
+  expect(submarine.isSunk).toBe(true);
+});
+
+test('calling isSunk returns false for hits < length', () => {
+  let submarine = shipFactory(3, 1);
+  submarine.isSunk = isSunk(submarine);
+  expect(submarine.isSunk).toBe(false);
 });
