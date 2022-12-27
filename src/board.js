2774709b594934b96197ffdc56b9281d290c0b70
Mike Rachman <mikerachman1@gmail.com>
2022-12-27 10:55:32 -0500

    wrote placePiece function //needs refactor

--- src/board.js
+++ src/board.js
@@ -3,8 +3,25 @@
 const gameBoardFactory = () => {
   let board = [...Array(10)].map(e => Array(10).fill(''));
   
-  
-  return { board };
+  const placePiece = (ship, startCoordinate, direction) => {
+    const shipLength = ship.length - 1;
+    board[startCoordinate[0]][startCoordinate[1]] = 'O';
+    if (direction === 'h') {
+      let shipLengthIncrementor = 0;
+      for (let i = 0; i < shipLength; i++) {
+        shipLengthIncrementor += 1
+        board[startCoordinate[0]][startCoordinate[1] + shipLengthIncrementor] = 'O';
+      }
+    } else {
+      let shipLengthIncrementor = 0;
+      for (let i = 0; i < shipLength; i++) {
+        shipLengthIncrementor += 1
+        board[startCoordinate[0] + shipLengthIncrementor][startCoordinate[1]] = 'O';
+      }
+    }
+  };
+
+  return { board, placePiece };
 };
 
 
