59e6a4435d3b74d0597b446b34d270971d5b4a1c
Mike Rachman <mikerachman1@gmail.com>
2022-12-27 10:55:17 -0500

    wrote tests for placePiece

--- src/board.test.js
+++ src/board.test.js
@@ -1,16 +1,29 @@
 /* eslint-disable */
 
 import { gameBoardFactory } from "./board";
+import { shipFactory } from "./ship";
 
 test('gameBoardFactory creates 10x10 game grid array', () => {
-  let board = gameBoardFactory().board;
-  expect(board.length).toBe(10);
-  expect(board[0].length).toBe(10);
-  expect(board[9].length).toBe(10);
-  expect(board[0][0]).toBe('');
+  let newBoard = gameBoardFactory();
+  expect(newBoard.board.length).toBe(10);
+  expect(newBoard.board[0].length).toBe(10);
+  expect(newBoard.board[9].length).toBe(10);
+  expect(newBoard.board[0][0]).toBe('');
 });
 
-test('placePiece puts piece on board', () => {
-
+test('placePiece puts small piece on board horizontally', () => {
+  let newBoard = gameBoardFactory();
+  const patrolBoat = shipFactory(2);
+  newBoard.placePiece(patrolBoat, [0,0], 'h');
+  expect(newBoard.board[0][0]).toBe('O');
+  expect(newBoard.board[0][1]).toBe('O');
 });
 
+test('placePiece puts large piece on board vertically', () => {
+  let newBoard = gameBoardFactory();
+  const carrier = shipFactory(5);
+  newBoard.placePiece(carrier, [3,3], 'v');
+  expect(newBoard.board[3][3]).toBe('O');
+  expect(newBoard.board[4][3]).toBe('O');
+  expect(newBoard.board[7][3]).toBe('O');
+});
