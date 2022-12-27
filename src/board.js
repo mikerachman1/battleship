2b02b994f68f0442fa926435e98b1a55f70ce81d
Mike Rachman <mikerachman1@gmail.com>
2022-12-27 10:05:37 -0500

    refactored gameBoardFactory

--- src/board.js
+++ src/board.js
@@ -1,6 +1,12 @@
+// '' for empty space, O for ship, X for ship hit, - for missed hit
+
 const gameBoardFactory = () => {
-  let board = [...Array(10)].map(e => Array(10));
-  return board;
-}
+  let board = [...Array(10)].map(e => Array(10).fill(''));
+  
+  
+  return { board };
+};
+
+
 
 export { gameBoardFactory };
--- src/board.test.js
+++ src/board.test.js
@@ -3,9 +3,14 @@
 import { gameBoardFactory } from "./board";
 
 test('gameBoardFactory creates 10x10 game grid array', () => {
-  let board = gameBoardFactory();
+  let board = gameBoardFactory().board;
   expect(board.length).toBe(10);
   expect(board[0].length).toBe(10);
   expect(board[9].length).toBe(10);
-})
+  expect(board[0][0]).toBe('');
+});
+
+test('placePiece puts piece on board', () => {
+
+});
 
