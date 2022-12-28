6ec8071fdf5281d7c233b1f00fbcb0f0275e5a81
Mike Rachman <mikerachman1@gmail.com>
2022-12-28 11:05:14 -0500

    refactored placePiece to handle placing conflicts

--- src/board.js
+++ src/board.js
@@ -4,18 +4,18 @@ const gameBoardFactory = () => {
   let board = [...Array(10)].map(e => Array(10).fill(''));
   
   const placePiece = (ship, startCoordinate, direction) => {
-    const shipLength = ship.length - 1;
+    if (board[startCoordinate[0]][startCoordinate[1]] !== '') { return 'Error, this placement conflicts with another piece'; }
     board[startCoordinate[0]][startCoordinate[1]] = 'O';
-    if (direction === 'h') {
-      let shipLengthIncrementor = 0;
-      for (let i = 0; i < shipLength; i++) {
-        shipLengthIncrementor += 1
+    const shipLength = ship.length - 1;
+    let shipLengthIncrementor = 0;
+    for (let i = 0; i < shipLength; i++) {
+      shipLengthIncrementor += 1;
+      if (direction === 'h') {
+        if (board[startCoordinate[0]][startCoordinate[1] + shipLengthIncrementor] !== '') { return 'Error, this placement conflicts with another piece'; }
         board[startCoordinate[0]][startCoordinate[1] + shipLengthIncrementor] = 'O';
       }
-    } else {
-      let shipLengthIncrementor = 0;
-      for (let i = 0; i < shipLength; i++) {
-        shipLengthIncrementor += 1
+      if (direction === 'v') {
+        if (board[startCoordinate[0] + shipLengthIncrementor][startCoordinate[1]] !== '') { return 'Error, this placement conflicts with another piece'; }
         board[startCoordinate[0] + shipLengthIncrementor][startCoordinate[1]] = 'O';
       }
     }
@@ -27,3 +27,21 @@ const gameBoardFactory = () => {
 
 
 export { gameBoardFactory };
+
+// const placePiece = (ship, startCoordinate, direction) => {
+//   board[startCoordinate[0]][startCoordinate[1]] = 'O';
+//   const shipLength = ship.length - 1;
+//   if (direction === 'h') {
+//     let shipLengthIncrementor = 0;
+//     for (let i = 0; i < shipLength; i++) {
+//       shipLengthIncrementor += 1
+//       board[startCoordinate[0]][startCoordinate[1] + shipLengthIncrementor] = 'O';
+//     }
+//   } else {
+//     let shipLengthIncrementor = 0;
+//     for (let i = 0; i < shipLength; i++) {
+//       shipLengthIncrementor += 1
+//       board[startCoordinate[0] + shipLengthIncrementor][startCoordinate[1]] = 'O';
+//     }
+//   }
+//  };
