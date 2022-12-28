fc6824bd18084754b6874a64e9a37f4f2c291fb4
Mike Rachman <mikerachman1@gmail.com>
2022-12-28 16:07:47 -0500

    rewrote tests to refactor code

--- src/ship.test.js
+++ src/ship.test.js
@@ -4,18 +4,18 @@ import { shipFactory, hit, isSunk } from "./ship";
 test('calling hit with ship returns new ship hit count', () => {
   let carrier = shipFactory(5);
   expect(carrier.hits).toBe(0);
-  carrier.hits = hit(carrier);
+  carrier.hit();
   expect(carrier.hits).toBe(1);
 });
 
 test('calling isSunk returns true for hits === length', () => {
   let submarine = shipFactory(3, 3);
-  submarine.isSunk = isSunk(submarine);
-  expect(submarine.isSunk).toBe(true);
+  submarine.isSunk()
+  expect(submarine.sunk).toBe(true);
 });
 
 test('calling isSunk returns false for hits < length', () => {
   let submarine = shipFactory(3, 1);
-  submarine.isSunk = isSunk(submarine);
-  expect(submarine.isSunk).toBe(false);
+  submarine.isSunk()
+  expect(submarine.sunk).toBe(false);
 });
