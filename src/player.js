535bda2895be7adf6ef5140011ea2677ce3a9349
Mike Rachman <mikerachman1@gmail.com>
2022-12-29 15:38:27 -0500

    added checkShipPlacement function

--- src/player.js
+++ src/player.js
@@ -6,6 +6,24 @@ class Player {
 
     this.attack = (opponent, attackCoords) => { opponent.gameboard.receiveAttack(attackCoords); };
   }
+
+  checkShipPlacement(ship, startCoordinate, direction) {
+    const gameBoard = this.gameboard.board;
+    if (gameBoard[startCoordinate[0]][startCoordinate[1]] !== '') { return false; }
+    const shipLength = ship.length - 1;
+    let shipLengthIncrementor = 0;
+    for (let i = 0; i < shipLength; i += 1) {
+      shipLengthIncrementor += 1;
+      let nextCoord = '';
+      if (direction === 'h') {
+        nextCoord = [startCoordinate[0], (startCoordinate[1] + shipLengthIncrementor)];
+      } else if (direction === 'v') {
+        nextCoord = [startCoordinate[0] + shipLengthIncrementor, startCoordinate[1]];
+      }
+      if (gameBoard[nextCoord[0]][nextCoord[1]] !== '') { return false; }
+    }
+    return true;
+  }
 }
 
 export { Player };
--- src/player.test.js
+++ src/player.test.js
@@ -4,10 +4,21 @@ import { Player } from "./player";
 
 describe('when a player is created', () => {
   const player = new Player;
+  const ship1 = player.gameboard.buildShip(5)
+  const ship2 = player.gameboard.buildShip(4);
 
   test('the player has a gameboard', () => {
     expect(player.gameboard).toBeTruthy;
   });
+
+  test('checkShipPlacement returns true if valid placement', () => {
+    expect(player.checkShipPlacement(ship1, [0, 0], 'v')).toBe(true);
+  });
+
+  test('checkShipPlacement returns false if not valid placement', () => {
+    player.gameboard.placePiece(ship1, [0,0], 'v');
+    expect(player.checkShipPlacement(ship2, [0,0], 'h')).toBe(false);
+  })
 });
 
 describe('when a player attacks', () => {
