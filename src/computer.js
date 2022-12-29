ff77a837562e73a2fb5a20a4ab49e268b77b54c7
Mike Rachman <mikerachman1@gmail.com>
2022-12-29 16:07:44 -0500

    refactored checkShipPlacement

---
+++ src/computer.js
@@ -0,0 +1,32 @@
+import { Player } from "./player.js";
+
+class Computer extends Player {
+  constructor() {
+    super();
+
+    this.randomInt = (maxVal) => Math.floor(Math.random() * maxVal);
+  }
+
+  randomPlacement(shipLength) {
+    let randomCoords = [this.randomInt(10), this.randomInt(10)];
+    let direction = this.randomInt(2) ? 'h' : 'v';
+    const ship = this.gameboard.buildShip(shipLength);
+    while (this.checkShipPlacement(ship, randomCoords, direction) === false) {
+      console.log('had to try again')
+      randomCoords = [this.randomInt(10), this.randomInt(10)];
+      direction = this.randomInt(2) ? 'h' : 'v';
+    }
+    this.gameboard.placePiece(ship, randomCoords, direction);
+    console.log('success')
+  }
+}
+
+export { Computer };
+
+const computer = new Computer;
+computer.randomPlacement(5);
+computer.randomPlacement(5);
+computer.randomPlacement(5);
+computer.randomPlacement(5);
+computer.randomPlacement(5);
+computer.randomPlacement(5);
